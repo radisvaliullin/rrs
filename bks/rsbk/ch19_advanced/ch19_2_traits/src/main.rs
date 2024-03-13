cf1eb4d1da52df057cd56949cd5ba14a2b3a2218
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:55 -0700

    bks: rsbk: ch19: ch19_2: external traits

--- bks/rsbk/ch19_advanced/ch19_2_traits/src/main.rs
+++ bks/rsbk/ch19_advanced/ch19_2_traits/src/main.rs
@@ -25,8 +25,20 @@ impl fmt::Display for Point {
 
 impl OutlinePrint for Point {}
 
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
 fn main() {
     let p = Point { x: 4, y: 32 };
     println!("p is:");
     p.outline_print();
+
+    //
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
