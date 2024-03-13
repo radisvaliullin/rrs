3a074c124128a244d50ad3608d742de96c8fdee8
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:54 -0700

    bks: rsbk: ch10: ch10_1: generic data types, update

--- bks/rsbk/ch10_generics/ch10_1_generic_data_types/src/main.rs
+++ bks/rsbk/ch10_generics/ch10_1_generic_data_types/src/main.rs
@@ -12,13 +12,25 @@ impl<X1, Y1> Point<X1, Y1> {
     }
 }
 
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
     println!("generic data types");
 
     let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "qwerty", y: 'c' };
-
+    let p2 = Point {
+        x: "qwerty",
+        y: 'c',
+    };
     let p3 = p1.mixup(p2);
-
     println!("p3 fields: p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    //
+    let p4 = Point { x: 2.0, y: 4.0 };
+    let res = p4.distance_from_origin();
+    println!("p4 - {}", res);
 }
