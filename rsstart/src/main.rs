075a97640d2f3398342da93f8756e544b548e222
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:53 -0700

    rsstart: add dependecy ferris says, update main

--- rsstart/src/main.rs
+++ rsstart/src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world! I'm Started Ыыы! Улыбочку!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Карлики!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
