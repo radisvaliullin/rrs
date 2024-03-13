025dc3f0762774d97369bca3e5fbc635e06b1c80
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:53 -0700

    bks: rsbk: ch6: ch6_1: enum define, update

--- bks/rsbk/ch6_enum/ch6_1_enum/src/main.rs
+++ bks/rsbk/ch6_enum/ch6_1_enum/src/main.rs
@@ -1,15 +1,21 @@
 #[derive(Debug)]
 enum Message {
     Quit,
-    // commented because get dead code worning
-    // Move { x: i32, y: i32 },
+    Move { x: i32, y: i32 },
     Write(String),
     ChangeColor(i32, i32, i32),
 }
 
 impl Message {
     fn print(&self) {
-        println!("message: {:#?}", self)
+        match &self {
+            Message::Move {x, y} => {
+                println!("message: {:#?}, {}, {}", &self, x, y)
+            },
+            _ => {
+                println!("message: {:#?}", &self)
+            }
+        }
     }
 }
 
@@ -18,8 +24,8 @@ fn main() {
 
     let mq = Message::Quit;
     mq.print();
-    // let mm = Message::Move{x: 2, y: 84};
-    // mm.print();
+    let mm = Message::Move{x: 2, y: 84};
+    mm.print();
     let mw = Message::Write(String::from("qwerty"));
     mw.print();
     let mcc = Message::ChangeColor(1, 2, 3);
