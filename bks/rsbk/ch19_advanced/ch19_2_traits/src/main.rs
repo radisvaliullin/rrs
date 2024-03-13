581595e9f5fda2231577cafcea45107d8cb69b1d
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:55 -0700

    bks: rsbk: ch19: ch19_2: advanced traits

--- bks/rsbk/ch19_advanced/ch19_2_traits/src/main.rs
+++ bks/rsbk/ch19_advanced/ch19_2_traits/src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::ops::Add;
 
 trait OutlinePrint: fmt::Display {
     fn outline_print(&self) {
@@ -12,6 +13,7 @@ trait OutlinePrint: fmt::Display {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
@@ -33,6 +35,77 @@ impl fmt::Display for Wrapper {
     }
 }
 
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Millimeters(u32);
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
 fn main() {
     let p = Point { x: 4, y: 32 };
     println!("p is:");
@@ -41,4 +114,26 @@ fn main() {
     //
     let w = Wrapper(vec![String::from("hello"), String::from("world")]);
     println!("w = {}", w);
+
+    //
+    let p1 = Point { x: 1, y: 0 };
+    let p2 = Point { x: 2, y: 3 };
+    let np = p1 + p2;
+    println!("new point = p + p = {:?} + {:?} = {:?}", p1, p2, np);
+
+    //
+    let mm = Millimeters(1024);
+    let m = Meters(1);
+    let nmm = mm + m;
+    println!("new mm = mm + m = {:?} + {:?} = {:?}", mm, m, nmm);
+
+    //
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+
+    //
+    println!("A baby dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
 }
