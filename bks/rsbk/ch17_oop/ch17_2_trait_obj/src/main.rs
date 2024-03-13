2de9ce6d99b1b99a168c56779a82a07c76b877e4
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:55 -0700

    bks: rsbk: ch17: ch17_2: oop, trait object

---
+++ bks/rsbk/ch17_oop/ch17_2_trait_obj/src/main.rs
@@ -0,0 +1,38 @@
+use ch17_2_trait_obj::Draw;
+use ch17_2_trait_obj::{Button, Screen};
+
+#[derive(Debug)]
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("select box: {:?}; width - {}, height - {}, options - {:?}", &self, self.width, self.height, self.options);
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
