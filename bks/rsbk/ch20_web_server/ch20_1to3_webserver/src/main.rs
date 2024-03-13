e9a05af964fdbb884ff09bb660436fc27ad48488
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:55 -0700

    bks: rsbk: ch20: ch20_2: web server, step 3, multi thread

--- bks/rsbk/ch20_web_server/ch20_1to3_webserver/src/main.rs
+++ bks/rsbk/ch20_web_server/ch20_1to3_webserver/src/main.rs
@@ -2,18 +2,24 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use bks_rsbk_ch20_webserver::ThreadPool;
 
 fn main() {
     println!("server: main");
 
     // listen
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -23,10 +29,15 @@ fn handle_connection(mut stream: TcpStream) {
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     // response
-    let (status_line, html_template_path) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "bks/rsbk/ch20_web_server/ch20_1to3_webserver/web_content/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "bks/rsbk/ch20_web_server/ch20_1to3_webserver/web_content/404.html")
+    let html_template_hello = "bks/rsbk/ch20_web_server/ch20_1to3_webserver/web_content/hello.html";
+    let html_template_404 = "bks/rsbk/ch20_web_server/ch20_1to3_webserver/web_content/404.html";
+    let (status_line, html_template_path) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", html_template_hello),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", html_template_hello)
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", html_template_404),
     };
     let contents = fs::read_to_string(html_template_path).unwrap();
     let length = contents.len();
