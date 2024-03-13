743bedcf808256f7a81ff1f862596c9bb31c63e2
Radis Valiullin <radisrower@gmail.com>
2024-03-13 16:29:54 -0700

    bks: rsbk: ch14: ch14_3: cargo workspace

---
+++ bks/rsbk/ch14_cargo_crates/ch14_3_workspaces/add_one/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+// add_one
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
