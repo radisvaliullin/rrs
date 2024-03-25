d5fcc079121dc963dfee3fcdf047d3e24142dde5
Radis Valiullin <radisrower@gmail.com>
2024-03-24 22:58:50 -0700

    play: rust playground

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,3 +1,15 @@
+use play::*;
+
+/// Add lib.rs if clone repo (ignored file)
+/// Add to lib.rs next code:
+/// ```
+/// pub fn mainmain() {
+///     println!("mainmain");
+/// }
+/// ```
+///
 fn main() {
     println!("playground");
+
+    mainmain();
 }
